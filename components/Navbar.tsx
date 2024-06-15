f777019396b42a2410b1a9bfcce37efd64238dae
Chandra Bose <chandrabosep3112@gmail.com>
2024-06-15 19:19:28 +0530

    feat:engage-form

---
+++ components/Navbar.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+import { Coustard } from "next/font/google";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+
+const coustard = Coustard({ weight: "400", subsets: ["latin"] });
+
+export default function Navbar() {
+	const path = usePathname();
+	return (
+		<div className={` bg-black w-full`}>
+			<div className="max-w-screen-2xl w-11/12 mx-auto py-4 flex items-center justify-between">
+				<div className="flex items-center gap-x-4 text-sm">
+					<Link href="/">
+						<Image
+							src={"/logo.svg"}
+							alt="logo"
+							width={500}
+							height={500}
+							className="w-48"
+						/>
+					</Link>
+					{[
+						{ title: "creator", href: "/creator" },
+						{ title: "play", href: "/play" },
+						{ title: "dashboard", href: "/dashboard" },
+					].map(({ title, href }, index) => (
+						<Link
+							key={index}
+							className={`${
+								path === href
+									? "bg-pink border-none"
+									: "bg-none"
+							} mx-4 text-pearl px-8 py-2 rounded-lg border border-pearl/30 ${
+								coustard.className
+							}`}
+							href={href}
+						>
+							{title}
+						</Link>
+					))}
+				</div>
+				<Button className="bg-transparent border border-pearl/50 text-sm">
+					Connect to Arcade
+				</Button>
+			</div>
+		</div>
+	);
+}
