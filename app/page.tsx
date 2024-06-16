fdcb5962a1109097536fef05019e69e4a13a8d91
Chandra Bose <chandrabosep3112@gmail.com>
2024-06-16 23:41:33 +0530

    chore:landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,190 @@
+"use client";
 import Image from "next/image";
-
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+} from "@/components/ui/carousel";
+import { coustard, unbounded } from "@/components/Fonts";
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<div className="max-w-screen-2xl w-11/12 mx-auto flex flex-col gap-y-10 py-8">
+			<div className="relative p-6  flex justify-between bg-purple text-cream border-black border-8 rounded-mg w-full">
+				<Image
+					src="/meme-arcade-hero.svg"
+					alt="alt"
+					width={500}
+					height={500}
+					className="absolute bottom-0 left-4 w-[15%]"
+				/>
+				<div className="flex flex-col h-full justify-between gap-y-4 z-10">
+					<h6 className={`${unbounded.className} text-6xl w-1/2 font-bold`}>
+						One Arcade to meme all!
+					</h6>
+					<p className={`${coustard.className} text-lg w-2/3`}>
+						Players all over the world, games from all over the
+						metaverse.
+					</p>
+				</div>
+				<div
+					className={`${unbounded.className} flex items-center gap-x-4 border-[6px] rounded-md text-purple-800 border-purple-600 bg-custom-gradient px-6 pr-10 py-4 text-3xl font-black w-[18rem]`}
+				>
+					More than 900 games to choose
+				</div>
+				<Image
+					src="/memearcade-logo.png"
+					alt="alt"
+					width={500}
+					height={500}
+					className="absolute bottom-0 right-0 w-28"
+				/>
+			</div>
+			<div className="flex items-center justify-evenly py-2">
+				<Image
+					src="/roadfighter-home.png"
+					alt="alt"
+					className="w-[30rem] h-fit border-l-[6px] border-y-[6px] border-purple-grey rounded-l"
+					width={1000}
+					height={1000}
+				/>
+				<div className="absolute flex flex-col w-[30rem] gap-y-2 pb-4 px-3 bg-pearl border-black border-4 rounded-md">
+					<p
+						className={`${coustard.className} text-lg font-bold pt-1.5 text-purple-800 text-center`}
+					>
+						Featured Games
+					</p>
+					<Carousel
+						// plugins={[
+						// 	Autoplay({
+						// 		delay: 4000,
+						// 	}),
+						// ]}
+						className="w-[28rem] "
+					>
+						<CarouselContent>
+							<CarouselItem>
+								<Image
+									src="/flappyBird.png"
+									alt="alt"
+									className="h-full w-full"
+									width={1000}
+									height={1000}
+								/>
+							</CarouselItem>
+							<CarouselItem>
+								<Image
+									src="/poker.png"
+									alt="alt"
+									className="h-full w-full"
+									width={1000}
+									height={1000}
+								/>
+							</CarouselItem>
+							<CarouselItem>
+								<Image
+									src="/carfighter.png"
+									alt="alt"
+									className="h-full w-full"
+									width={1000}
+									height={1000}
+								/>
+							</CarouselItem>
+						</CarouselContent>
+					</Carousel>
+					<Image
+						src="/player.svg"
+						alt="alt"
+						className="w-[33rem] h-fit"
+						width={1000}
+						height={1000}
+					/>
+				</div>
+				<Image
+					src="/poker-home.png"
+					alt="alt"
+					className="w-[30rem] h-fit border-r-[6px] border-y-[6px] border-purple-grey rounded-r"
+					width={1000}
+					height={1000}
+				/>
+			</div>
+			<div className="grid grid-cols-2 py-16 justify-items-center">
+				<div className=" flex flex-col gap-y-6 bg-pearl border-4 border-purple-grey-800 w-[80%] p-6 rounded-lg">
+					<div className="bg-purple text-cream p-4 rounded-md flex items-center justify-between">
+						<p
+							className={`${unbounded.className} text-xl font-bold`}
+						>
+							Player
+						</p>
+						<Image
+							src="/icon-silver.svg"
+							alt="alt"
+							width={500}
+							height={500}
+							className="w-5"
+						/>
+					</div>
+					<div className="flex gap-x-6 text-purple-800">
+						<Image
+							src="/pepe-player.png"
+							alt="alt"
+							width={500}
+							height={500}
+							className="w-1/2 border-4 border-purple-grey-800 rounded-lg"
+						/>
+						<div className="flex flex-col gap-y-6">
+							<p
+								className={`${coustard.className} text-2xl font-medium`}
+							>
+								Get rewarded for playing and growing the meme
+								community.
+							</p>
+							<h6
+								className={`${coustard.className} text-4xl font-black`}
+							>
+								More than $500.000 usd for grabs.
+							</h6>
+						</div>
+					</div>
+				</div>
+				<div className=" flex flex-col gap-y-6 bg-pearl border-4 border-purple-grey-800 w-[80%] p-6 rounded-lg">
+					<div className="bg-purple text-cream p-4 rounded-md flex items-center justify-between">
+						<p
+							className={`${unbounded.className} text-xl font-bold`}
+						>
+							Creator
+						</p>
+						<Image
+							src="/icon-silver.svg"
+							alt="alt"
+							width={500}
+							height={500}
+							className="w-5"
+						/>
+					</div>
+					<div className="flex gap-x-6 text-purple-800">
+						<Image
+							src="/pepe-creator.png"
+							alt="alt"
+							width={500}
+							height={500}
+							className="w-1/2 border-4 border-purple-grey-800 rounded-lg"
+						/>
+						<div className="flex flex-col gap-y-6">
+							<p
+								className={`${coustard.className} text-2xl font-medium`}
+							>
+								Reward user to grow your community, improve
+								liquidity and keep pumpin.
+							</p>
+							<h6
+								className={`${coustard.className} text-4xl font-black`}
+							>
+								More than 80 projects have joined the Arcade.
+							</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
