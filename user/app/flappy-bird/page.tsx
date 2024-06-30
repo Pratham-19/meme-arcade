7e77158a55ecaa3737ef18300b97ba8696dacf46
Pratham <prathamsinghal2003@gmail.com>
2024-06-30 23:14:33 +0530

    feat: add touch

--- user/app/flappy-bird/page.tsx
+++ user/app/flappy-bird/page.tsx
@@ -1,12 +1,11 @@
 "use client";
-
-import { useInterval } from "@/hooks/useInterval";
-import { useStore } from "@/hooks/useStore";
-import { usePoints } from "@/store/player";
-import { ArrowLeft } from "lucide-react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState, useCallback, useRef } from "react";
+import { ArrowLeft } from "lucide-react";
+import { useStore } from "@/hooks/useStore";
+import { usePoints } from "@/store/player";
+import { useInterval } from "@/hooks/useInterval";
 
 interface Pillar {
   top: number;
@@ -150,15 +149,35 @@ export default function FloppyBird() {
     }
   }, []);
 
+  const handleTouchStart = useCallback(() => {
+    isJumpingRef.current = true;
+  }, []);
+
+  const handleTouchEnd = useCallback(() => {
+    isJumpingRef.current = false;
+  }, []);
+
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
     window.addEventListener("keyup", handleKeyUp);
 
+    const gameArea = document.querySelector(".game-area");
+
+    if (gameArea) {
+      gameArea.addEventListener("touchstart", handleTouchStart);
+      gameArea.addEventListener("touchend", handleTouchEnd);
+    }
+
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
       window.removeEventListener("keyup", handleKeyUp);
+
+      if (gameArea) {
+        gameArea.removeEventListener("touchstart", handleTouchStart);
+        gameArea.removeEventListener("touchend", handleTouchEnd);
+      }
     };
-  }, [handleKeyDown, handleKeyUp]);
+  }, [handleKeyDown, handleKeyUp, handleTouchStart, handleTouchEnd]);
 
   useInterval(() => {
     if (!gameOver) {
